a701ef8cf1e17c42460affaeccc546fb054553c3
Anan Afrida <anan.afrida.yes@gmail.com>
2023-08-25 23:45:50 -0400

    showing

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ async function initMap() {
   map = new Map(document.getElementById("map"), {
     center: { lat: 41.556240724638144, lng: -72.65683037211356 }, 
     zoom: 15,
+    mapId: "c60343d395077993", 
     fullscreenControl: true,
-    streetViewControl: false, 
+    streetViewControl: true, 
     mapTypeControl: false,
   });
 
