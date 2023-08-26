7f01c70df6dd47ba601946b0911c848b71224c23
Anan Afrida <anan.afrida.yes@gmail.com>
2023-08-25 22:49:38 -0400

    just id

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import { busStops } from "./busStops";
-import { parkingLots } from "./parkingLots";
-import { dining } from "./dining";
-import { compost } from "./compost";
-import { bikeParking } from "./bikeParking";
-import { academics } from "./academics";
-import { athletics } from "./athletics";
 
 // $(document).ready(function () {
 
@@ -17,7 +10,6 @@ import { athletics } from "./athletics";
 // [START maps_map_simple]
 let map;
 
-var markers = [];
 async function initMap() {
   
   const { Map } = await google.maps.importLibrary("maps");
@@ -25,20 +17,10 @@ async function initMap() {
   map = new Map(document.getElementById("map"), {
     center: { lat: 41.556240724638144, lng: -72.65683037211356 }, 
     zoom: 15,
-    mapId: "c60343d395077993", 
     fullscreenControl: true,
     streetViewControl: false, 
     mapTypeControl: false,
   });
-
-  parkingLots(map, markers);
-  busStops(map, markers);
-  dining(map, markers);
-  compost(map, markers);
-  bikeParking(map, markers);
-  academics(map, markers);
-  athletics(map, markers);
-  console.log(markers);
 
 }
 initMap();
